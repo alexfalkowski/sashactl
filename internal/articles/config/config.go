faa93fb96d41f3985283ac75928038ee5bf2884f
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-05-04 11:40:24 +0200

    feat(article): expand path (#25)

--- internal/articles/config/config.go
+++ internal/articles/config/config.go
@@ -1,6 +1,13 @@
 package config
 
+import "github.com/alexfalkowski/go-service/os"
+
 // Config for articles.
 type Config struct {
 	Path string `yaml:"path,omitempty" json:"path,omitempty" toml:"path,omitempty"`
+}
+
+// GetPath from config.
+func (c *Config) GetPath() string {
+	return os.ExpandPath(c.Path)
 }
