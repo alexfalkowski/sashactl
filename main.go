4f9c96de50476efb1e9cbcb2a9a74050f01f9299
Alex Falkowski <alexrfalkowski@gmail.com>
2025-05-19 13:33:31 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.614.0
    https://github.com/alexfalkowski/go-service/pull/1608

--- main.go
+++ main.go
@@ -4,26 +4,19 @@ import (
 	"context"
 
 	"github.com/alexfalkowski/go-service/cmd"
-	"github.com/alexfalkowski/go-service/env"
-	"github.com/alexfalkowski/go-service/os"
 	"github.com/alexfalkowski/sashactl/internal/cmd/delete"
 	"github.com/alexfalkowski/sashactl/internal/cmd/new"
 	"github.com/alexfalkowski/sashactl/internal/cmd/publish"
 	"github.com/alexfalkowski/sashactl/internal/cmd/unpublish"
 )
 
-func main() {
-	command().ExitOnError(context.Background())
-}
-
-func command() *cmd.Command {
-	fs := os.NewFS()
-	command := cmd.New(env.NewName(fs), env.NewVersion())
-
+var app = cmd.NewApplication(func(command *cmd.Command) {
 	delete.Register(command)
 	new.Register(command)
 	publish.Register(command)
 	unpublish.Register(command)
+})
 
-	return command
+func main() {
+	app.ExitOnError(context.Background())
 }
