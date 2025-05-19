70a21be2a1e111bce32ec11a95c06fd0150e633a
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-05-19 07:52:28 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.613.0 (#81)
    https://github.com/alexfalkowski/go-service/pull/1607

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/alexfalkowski/go-service/cmd"
 	"github.com/alexfalkowski/go-service/env"
+	"github.com/alexfalkowski/go-service/os"
 	"github.com/alexfalkowski/sashactl/internal/cmd/delete"
 	"github.com/alexfalkowski/sashactl/internal/cmd/new"
 	"github.com/alexfalkowski/sashactl/internal/cmd/publish"
@@ -16,7 +17,8 @@ func main() {
 }
 
 func command() *cmd.Command {
-	command := cmd.New(env.NewName(), env.NewVersion())
+	fs := os.NewFS()
+	command := cmd.New(env.NewName(fs), env.NewVersion())
 
 	delete.Register(command)
 	new.Register(command)
